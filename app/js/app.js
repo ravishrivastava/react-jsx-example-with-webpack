7b8e02b8225ab7b7b4087e3478c7486583db2f5d
Ravi Shrivastava <rsravishri30@gmail.com>
2018-07-16 10:21:28 +0530

    added page not found exception

--- app/js/app.js
+++ app/js/app.js
@@ -4,12 +4,14 @@ import { Router, Route, browserHistory } from 'react-router'
 import Layout from './components/Layout';
 import Timer from './components/Timer';
 import FilterableNameList from './components/FilterableNameList';
+import PageNotFound from './components/PageNotFound';
 
 ReactDOM.render(
 	<Router history={ browserHistory }>
 		<Route path="/" component={Layout} >
 			<Route path="timer" component={Timer} />
 			<Route path="namelist" component={FilterableNameList} />
+			<Route path="*" component={PageNotFound} />
 		</Route>
 	</Router>,
 	document.getElementById('app')
---
+++ app/js/components/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class PageNotFound extends React.Component {
+	constructor(){
+		super();
+	}
+	render(){
+		return(
+			<h2>Sorry, this page doesn't exist :( </h2>
+		)
+	}
+}
